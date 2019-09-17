c9b770244296dddbb76c1ee27ead8c4ea0f693d0
Suravi Sasadara Jayaweer <sasadara_j@epic.lk>
2019-09-17 14:57:39 +0530

    hibernate set up

---
+++ Login/src/com/sasadara/hibernate/demo/CreateDemo.java
@@ -0,0 +1,78 @@
+package com.sasadara.hibernate.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.sasadara.model.Student;
+
+
+
+
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration()
+								.configure("hibernate.cfg.xml")
+								.addAnnotatedClass(Student.class)
+								
+								.buildSessionFactory();
+		
+		// create session
+		Session session = factory.getCurrentSession();
+		
+		try {			
+			
+			// create the objects
+			/*
+			Instructor tempInstructor = 
+					new Instructor("Chad", "Darby", "darby@luv2code.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.luv2code.com/youtube",
+							"Luv 2 code!!!");		
+			*/
+			Student st = new Student("Suravi","sasadara","jayaweera", "sasaara@gmail.com","9091", "sasadarattt",
+			"admin");
+			/*Instructor tempInstructor = 
+					new Instructor("Madhu", "Patel", "madhu@luv2code.com");
+			
+			InstructorDetail tempInstructorDetail =
+					new InstructorDetail(
+							"http://www.m.youtube.com",
+							"Guitar");	*/	
+			
+			// associate the objects
+			//tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
+			// start a transaction
+			session.beginTransaction();
+			
+			// save the instructor
+			//
+			// Note: this will ALSO save the details object
+			// because of CascadeType.ALL
+			//
+			System.out.println("Saving instructor: " + st);
+			session.save(st);					
+			
+			// commit transaction
+			session.getTransaction().commit();
+			
+			System.out.println("Done!");
+		}
+		finally {
+			factory.close();
+		}
+	}
+
+}
+
+
+
+
+
---
+++ Login/src/com/sasadara/model/Student.java
@@ -0,0 +1,130 @@
+package com.sasadara.model;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user_table")
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="firstName")
+	private String firstName1;
+	
+	@Column(name="middleName")
+	private String middleName;
+	
+	@Column(name="lastName")
+	private String lastName;
+	
+	@Column(name="email")
+	public String email;
+	
+	@Column(name="userId")
+	public String userId;
+	
+	@Column(name="password")
+	public String password;
+	
+	@Column(name="Role")
+	public String Role;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName1() {
+		return firstName1;
+	}
+
+	public void setFirstName1(String firstName1) {
+		this.firstName1 = firstName1;
+	}
+
+	public String getMiddleName() {
+		return middleName;
+	}
+
+	public void setMiddleName(String middleName) {
+		this.middleName = middleName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUserId() {
+		return userId;
+	}
+
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getRole() {
+		return Role;
+	}
+
+	public void setRole(String role) {
+		Role = role;
+	}
+
+	public Student() {
+		
+	}
+
+	public Student(String firstName1, String middleName, String lastName, String email, String userId, String password,
+			String role) {
+		super();
+		this.firstName1 = firstName1;
+		this.middleName = middleName;
+		this.lastName = lastName;
+		this.email = email;
+		this.userId = userId;
+		this.password = password;
+		Role = role;
+	}
+
+
+	
+
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", firstName=" + firstName1 + ", lastName=" + lastName + ", email=" + email + "]";
+	}
+	
+}
+					
+
+
